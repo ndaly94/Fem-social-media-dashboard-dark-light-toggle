e6802c29facbee352bf0c5f30e64093c4c9ac09b
NDaly94 <NDaly94@gmail.com>
2023-05-14 14:42:06 -0400

    done with video, time to trobuleshoot!

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,56 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
 
-document.getElementById('dark').checked 
-? (document.querySelector('body').classList = 'dark') 
-: (document.querySelector('body').classList = 'light');
+
+const setColorMode = () => {
+
+    if  (localStorage.getItem('colorMode') == 'dark') {
+        setDarkMode();
+        darkButton.click();
+    }  else if (localStorage.getItem('colorMode') == 'light'){
+    setLightMode();
+    lightButton.click();
+    }
+};
+
+const checkMode = () => {
+    if (localStorage.getItem('colorMode') == null){
+    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+        lightButton.click();
+    } else if (window.matchMedia('(prefers-color-scheme: light)').matches){
+        darkButton.click();
+        }}
+};
+
+const checkModeChange = () => {
+    window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (event) => {
+         checkMode();
+    })
+}
+const setDarkMode = () => {
+    document.querySelector('body').classList = 'Dark';
+};
+
+const setLightMode = () => {
+    document.querySelector('body').classList = 'light';
+};
+
+setColorMode();
+checkMode();
+checkModeChange();
+
+const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+for (i=0; i < radioButtons.length; i++){
+    radioButtons[i].addEventListener('click', event => {
+        
+       if (darkButton.checked) {
+        document.querySelector('body').classList = 'dark';
+        setDarkMode();
+       } else {
+        localStorage.setItem('colormode', 'light');
+        setLightMode();
+        }
+    });
+};
